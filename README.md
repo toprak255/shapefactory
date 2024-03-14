79de0fb53664121f0fc13db02e02186614ec96b2
toprak <toprakaydiner6@gmail.com>
2024-03-14 14:17:12 +0300

    fixes

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# shapefactory
+
+# Shapefactory
+
+A Rust app for creating regular n-gons according to given parameters
+
+## Build
+
+```
+https://github.com/toprak255/shapefactory.git
+
+cd shapefactory
+
+cargo build --release
+```
+## Documentation
+After building you can run executable to display all available commands ,output should look like this
+```
+usage: shapefactory.exe [-corner <corner count>] [-Width <canvas-width>] [-Height <canvas-height>] [-scale <value>] 
+                        [-random-scale] [-random-rotation] [-fg <hex-32bit>] [-bg <hex-32bit>] [-f <folderPath>] [-n <filename>] 
+                        [-count <number of images>] [-random-color]
+```
+-fg and -bg isn't working as of now and are just ignored
+
+Every argument other than -corner -count and -filename has default "usable" values and program can work without them being set by user
+
+```
+width:1001
+height:1001
+scale:1.
+random_rotation:false,
+random_scale:false,
+random_color:false,
+foreground_color:Rgba([255,255,255,255])
+background_color:Rgba([0,0,0,255]),
+```
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
 
 
 use image::{ImageBuffer, Rgba};
-
 use rand::prelude::*;
 use std::env;
 
 mod stuff;
 use stuff::*;
-// fn draw_disc(img: &mut ImageBuffer<Rgba<u8>, Vec<u8>>, center: &Vec2, radius: f32) {
-//     for (x, y, pixel) in img.enumerate_pixels_mut() {
-//         let normalized_position = normalize(&x, &y);
-//         if get_distance(&normalized_position, &center) < radius {
-//             *pixel = Rgba([255, 255, 255, 255]);
-//         } else {
-//             *pixel = Rgba([60, 60, 60, 255]);
-//         }
-//     }
-// }
 
-//add background color input
 fn draw(img: &mut ImageBuffer<Rgba<u8>, Vec<u8>>, shape:&mut Shape){
     let bg= shape.background_color;
     let fg;
@@ -58,12 +46,12 @@ fn save_image(img: &ImageBuffer<Rgba<u8>, Vec<u8>>, folder_path: Option<&String>
 }
 
 pub fn main() {
-    let help_text:&str=("usage: shapefactory.exe [-corner <corner count>] [-Width <canvas-width>] [-Height <canvas-height>] [-scale <value>] 
-                        [-random-scale] [-random-rotation] [-fg <hex-32bit>] [-bg <hex-32bit>] [-f <folderPath>] [-n <filename>] 
+    let help_text:&str="usage: shapefactory.exe [-corner <corner count>] [-Width <canvas-width>] [-Height <canvas-height>] [-scale <value>] 
+                        [-random-scale] [-random-rotation] [-f <folderPath>] [-n <filename>] 
                         [-count <number of images>] [-random-color]
                         
                         providing filename and corner count is necassary, other values have defaults that can optionally be changed
-                        ");
+                        ";
     let args: Vec<String> = env::args().collect();
     if args.len()<=1{
         println!("{}",help_text);
@@ -76,11 +64,6 @@ pub fn main() {
     let mut base_filename =String::new();
     let mut folder_path =String::from(".");
     for i in 1..args.len(){
-        if args.len()<i+1{
-            println!("a possible flag has been entered but no value has been given");
-            std::process::exit(0);
-        }
-        
         match args[i].as_str(){
             //"-h" => {println!("{}",help_text);std::process::exit(0);}
             "-corner"=> {shape.corner_count=args[i+1].parse::<u32>().unwrap();}
@@ -91,32 +74,11 @@ pub fn main() {
             "-random-rotation" => {shape.random_rotation=true;}
             "-random-color" => {shape.random_color=true}
             "-count"=>{img_count=args[i+1].parse::<u32>().unwrap();}
-            "-fg"=>{
-                let val =&args[i+1];
-                let color:String;
-                if val.starts_with("0x"){
-                    if let Some(stripped_val) = val.strip_prefix("0x") {
-                        color = stripped_val.to_string();
-                }
-                else {color=val.to_string()};
-                if let Some(rgba_color) = to_rgb(color.as_str()){
-                shape.foreground_color=rgba_color;}
-                }}
-            "-bg"=>{
-                let val =&args[i+1];
-                let color:String;
-                if val.starts_with("0x"){
-                    if let Some(stripped_val) = val.strip_prefix("0x") {
-                        color = stripped_val.to_string();
-                }
-                else {color=val.to_string()};
-                if let Some(rgba_color) = to_rgb(color.as_str()){
-                shape.background_color=rgba_color;}
-                }}
+            "-fg"=>{}
+            "-bg"=>{}
             "-f" => {folder_path=String::from(&args[i+1])}
             "-n" => {base_filename=String::from(&args[i+1])}
             
-
             _ => {}
         }
     }
